c938043b4de43c5a911cca362bd106490a49c411
Prathamesh date <102506898+Prathamesh4545@users.noreply.github.com>
2025-03-19 19:11:44 +0530

    updated backend

---
+++ ShoppingBackend/src/main/java/com/prathamesh/ShoppingBackend/model/Address.java
@@ -0,0 +1,41 @@
+package com.prathamesh.ShoppingBackend.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "address")
+public class Address {
+
+    public enum AddressType {
+        SHIPPING,
+        BILLING
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String street;
+    private String city;
+    private String state;
+
+    @Column(name = "zip_code")
+    private String zipCode;
+
+    private String country;
+
+    @Enumerated(EnumType.STRING)
+    private AddressType type;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonBackReference
+    private User user;
+}
---
+++ ShoppingBackend/src/main/java/com/prathamesh/ShoppingBackend/repository/UserRepo.java
@@ -0,0 +1,13 @@
+package com.prathamesh.ShoppingBackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.prathamesh.ShoppingBackend.model.User;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+
+    User findByUserName(String username);
+
+}
