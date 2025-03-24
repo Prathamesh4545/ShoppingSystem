d831567e77f8ccbe331ee51fba0bf6c560750fbf
Prathamesh date <102506898+Prathamesh4545@users.noreply.github.com>
2025-03-24 18:59:43 +0530

    Delete ShoppingBackend directory
    delete because of new update gone be push soon

--- ShoppingBackend/src/main/java/com/prathamesh/ShoppingBackend/Exception/InvalidImageException.java
+++
@@ -1,7 +0,0 @@
-package com.prathamesh.ShoppingBackend.Exception;
-
-public class InvalidImageException extends Exception {
-    public InvalidImageException(String message){
-        super(message);
-    }
-}
