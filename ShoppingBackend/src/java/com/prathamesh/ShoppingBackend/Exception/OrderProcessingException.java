b7f983e531bda6fa56242b2cdf8213f6d1588491
Prathamesh date <102506898+Prathamesh4545@users.noreply.github.com>
2025-08-29 19:21:07 +0530

    Delete ShoppingBackend/src/java/com/prathamesh/ShoppingBackend directory

--- ShoppingBackend/src/java/com/prathamesh/ShoppingBackend/Exception/OrderProcessingException.java
+++
@@ -1,7 +0,0 @@
-package com.prathamesh.ShoppingBackend.Exception;
-
-public class OrderProcessingException extends RuntimeException {
-    public OrderProcessingException(String message) {
-        super(message);
-    }
-}
--- ShoppingBackend/src/java/com/prathamesh/ShoppingBackend/Exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.prathamesh.ShoppingBackend.Exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
